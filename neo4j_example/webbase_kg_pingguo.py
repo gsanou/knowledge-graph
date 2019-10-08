e0a4800e2416efedef4d12bc529d6b7e370919d4
Yuhao Mao <maoyuhao92@gmail.com>
2019-10-08 16:38:41 +0800

    pingguo level2
    test pingguo level 2 pass

--- neo4j_example/webbase_kg_pingguo.py
+++ neo4j_example/webbase_kg_pingguo.py
@@ -7,9 +7,11 @@ graph = Graph('http://localhost:7474', username='neo4j', password='0905')
 graph.delete_all()
 reg = "[^0-9A-Za-z\u4e00-\u9fa5]"  ####### remove symbol rule
 level=1
+# current_node = {}
 
 def web_kg(entity):
     global level
+    global current_node
     url = 'https://api.ownthink.com/kg/knowledge?entity=%s' % entity
     # print("搜索的url是：")
     # print(url)
@@ -35,10 +37,23 @@ def web_kg(entity):
 
         count_root = 1
         l = locals()
-        root_node = Node(entity, name=entity)
+        if level == 1:
+            root_node = Node(entity, name=entity)
+        else:
+            root_node = current_node
         for node in nodes:
+            # create node
             l['test_node_' + str(count_root)] = Node(entity, name=node.get("target"))
+            current_node = l['test_node_' + str(count_root)]
             graph.create(l['test_node_' + str(count_root)])
+            print("当前node 是")
+            print(current_node)
+            print(root_node)
+            print(type(root_node))
+            # create relationship
+            l['node_' + str(count_root) + "_root_node"] = Relationship(root_node, node.get("rela"),l['test_node_' + str(count_root)])
+            graph.create(l['node_' + str(count_root) + "_root_node"])
+            count_root += 1
 
             child_node_string = node.get("target")                  ####### child node create
             result = jieba.cut(child_node_string, HMM=False)        ####### jieba cut
@@ -55,18 +70,20 @@ def web_kg(entity):
                 web_kg(first_word)
             else:
                 print("当前level > 2")
-                level = 1
+                # level = 1
                 continue
-            if level == 2:
-                level = 1
+    else:
+        level -= 1
+            # if level == 2:
+            #     level = 1
 
-            l['node_' + str(count_root) +"_root_node"] = Relationship(root_node, node.get("rela"), l['test_node_' + str(count_root)])
-            graph.create(l['node_' + str(count_root) +"_root_node"])
+            # l['node_' + str(count_root) +"_root_node"] = Relationship(root_node, node.get("rela"), l['test_node_' + str(count_root)])
+            # graph.create(l['node_' + str(count_root) +"_root_node"])
 
-            node = str(node)
-            node = node.replace("'type'", 'type').replace("'source'", 'source').replace("'target'", 'target')
-            print(node + ',')
-            count_root += 1
+            # node = str(node)
+            # node = node.replace("'type'", 'type').replace("'source'", 'source').replace("'target'", 'target')
+            # print(node + ',')
+            # count_root += 1
 
 if __name__ == '__main__':
     web_kg('水果')
