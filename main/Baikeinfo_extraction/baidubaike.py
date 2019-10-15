aeadd0438a37aa7e8a680caafbc960927f1457ed
Yuhao Mao <maoyuhao92@gmail.com>
2019-10-15 16:35:51 +0800

    百度百科接口
    第十行的delete all 以后得删，还没写更新的接口

---
+++ main/Baikeinfo_extraction/baidubaike.py
@@ -0,0 +1,89 @@
+from urllib import request
+from lxml import etree
+import codecs
+from urllib import parse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from py2neo import Graph, Node, Relationship
+graph = Graph('http://localhost:7474', username='neo4j', password='0905')
+graph.delete_all()
+
+class BaiduBaike():
+    def __init__(self):
+        pass
+
+    def get_html(self, url):
+        return request.urlopen(url).read().decode('utf-8').replace('&nbsp;', '')
+
+    def info_extract_baidu(self, word):
+        url = "http://baike.baidu.com/item/%s" % parse.quote(word)
+        print(url)
+        selector = etree.HTML(self.get_html(url))
+        info_list = list()
+        info_list.append(self.extract_baidu(selector))
+        polysemantics = self.checkbaidu_polysemantic(selector)
+        if polysemantics:
+            info_list += polysemantics
+        infos = [info for info in info_list if len(info) > 2]
+        print(infos)
+        return infos
+
+    def extract_baidu(self, selector):
+        info_data = {}
+        # if selector.xpath('//h2/text()'):
+        #     info_data['current_semantic'] = selector.xpath('//h2/text()')[0].replace('    ', '').replace('（','').replace('）','')
+        # else:
+        #     info_data['current_semantic'] = ''
+        # if info_data['current_semantic'] == '目录':
+        #     info_data['current_semantic'] = ''
+
+        # info_data['tags'] = [item.replace('\n', '') for item in selector.xpath('//span[@class="taglist"]/text()')]
+        if selector.xpath("//div[starts-with(@class,'basic-info')]"):
+            for li_result in selector.xpath("//div[starts-with(@class,'basic-info')]")[0].xpath('./dl'):
+                attributes = [attribute.xpath('string(.)').replace('\n', '') for attribute in li_result.xpath('./dt')]
+                values = [value.xpath('string(.)').replace('\n', '') for value in li_result.xpath('./dd')]
+                for item in zip(attributes, values):
+                    info_data[item[0].replace('    ', '')] = item[1].replace('    ', '')
+        return info_data
+
+    def checkbaidu_polysemantic(self, selector):
+        semantics = ['https://baike.baidu.com' + sem for sem in
+                     selector.xpath("//ul[starts-with(@class,'polysemantList-wrapper')]/li/a/@href")]
+        names = [name for name in selector.xpath("//ul[starts-with(@class,'polysemantList-wrapper')]/li/a/text()")]
+        info_list = []
+        if semantics:
+            for item in zip(names, semantics):
+                selector = etree.HTML(self.get_html(item[1]))
+                info_data = self.extract_baidu(selector)
+                # info_data['current_semantic'] = item[0].replace('    ', '').replace('（','').replace('）','')
+                if info_data:
+                    info_list.append(info_data)
+        # print(info_list)
+        return info_list
+
+
+baidu = BaiduBaike()
+while(1):
+    word = input('enter an word:')
+    contentss = baidu.info_extract_baidu(word)
+    root_node = Node("Root_"+word, name=word)
+    count_root = 1
+    l = locals()
+    for contents in contentss:
+        print("contents:")
+        print(contents)
+        print(type(contents))
+        for content in contents:
+
+            # create node
+            l['test_node_' + str(count_root)] = Node(word, name=contents[content])
+            graph.create(l['test_node_' + str(count_root)])
+
+            # create relationship
+            l['node_' + str(count_root) + "_root_node"] = Relationship(root_node, content, l['test_node_' + str(count_root)])
+            graph.create(l['node_' + str(count_root) + "_root_node"])
+
+
+# [{'current_semantic': '钱钟书同名作品', 'tags': ['文学作品', '字词', '书籍', '中国文学'], '中文名': '吃饭', '作者': '钱钟书', '字': '默存', '号': '槐聚'},
+#  {'current_semantic': '章小东著图书', 'tags': ['出版物', '书籍'], '书名': '吃饭', '作者': '章小东', 'ISBN': '978-7-208-11349-7/1·1122', '页数': '156页', '定价': '32.00元', '出版社': '上海人民出版社', '出版时间': '2013年8月', '插页': '2', '字数': '225.000'}]
---
+++ neo4j_example/test.py
@@ -0,0 +1,111 @@
+import requests
+import re
+import jieba
+from py2neo import Graph, Node, Relationship
+
+g = Graph('http://localhost:7474', username='neo4j', password='0905')
+g.delete_all()
+# tx = graph.begin()
+# a = Node("person", name = "AAA")
+# tx.create(a)
+# b = Node("person", name = "BBB")
+# tx.create(b)
+#
+# ab = Relationship(a,"不认识",b)
+# tx.create(ab)
+# tx.commit()
+# graph.exit(ab)
+
+
+tx = g.begin()
+a = Node("明星", name="张鹤伦")
+tx.create(a)
+b = Node("明星", name="杨九郎")
+ab = Relationship(a, "师兄弟", b)
+tx.create(ab)
+tx.commit()
+g.exists(ab)
+
+
+# reg = "[^0-9A-Za-z\u4e00-\u9fa5]"  ####### remove symbol rule
+# level=1
+# # current_node = {}
+#
+# def web_kg(entity):
+#     global level
+#     global current_node
+#     url = 'https://api.ownthink.com/kg/knowledge?entity=%s' % entity
+#     # print("搜索的url是：")
+#     # print(url)
+#     sess = requests.get(url)
+#     text = sess.text
+#     # print("搜索到的text是：")
+#     # print(text)
+#     response = eval(text)
+#     knowledge = response['data']
+#     print("现在搜索得到的knowledge是：")
+#     print(knowledge)
+#     # if not knowledge:
+#     #     print("knowledge = empty")
+#     # if knowledge:
+#     #     print("knowledge != empty")
+#     nodes = []
+#     if knowledge:
+#         for avp in knowledge['avp']:
+#             # if avp[1] == knowledge['entity']:
+#             #     continue
+#             node = {'source': knowledge['entity'], 'target': avp[1], 'type': "resolved", 'rela': avp[0]}
+#             nodes.append(node)
+#
+#         count_root = 1
+#         l = locals()
+#         if level == 1:
+#             root_node = Node(entity, name=entity)
+#         else:
+#             root_node = current_node
+#         for node in nodes:
+#             # create node
+#             l['test_node_' + str(count_root)] = Node(entity, name=node.get("target"))
+#             current_node = l['test_node_' + str(count_root)]
+#             graph.create(l['test_node_' + str(count_root)])
+#             print("当前node 是")
+#             print(current_node)
+#             print(root_node)
+#             print(type(root_node))
+#             # create relationship
+#             l['node_' + str(count_root) + "_root_node"] = Relationship(root_node, node.get("rela"),l['test_node_' + str(count_root)])
+#             graph.create(l['node_' + str(count_root) + "_root_node"])
+#             count_root += 1
+#
+#             child_node_string = node.get("target")                  ####### child node create
+#             result = jieba.cut(child_node_string, HMM=False)        ####### jieba cut
+#             tmp = " ".join(result)                                  ####### jieba cut
+#             child_node_string = re.sub(reg, ' ', child_node_string) ####### remove symbols
+#             first_word = re.split(r'\s', child_node_string)[0]      ####### first word in targert
+#             print("第一个词语是：")
+#             print(first_word)
+#             print("entity 是：")
+#             print(entity)
+#             if level < 2 and first_word != entity:
+#                 print("当前level < 2")
+#                 level += 1
+#                 web_kg(first_word)
+#             else:
+#                 print("当前level > 2")
+#                 # level = 1
+#                 continue
+#     else:
+#         level -= 1
+#             # if level == 2:
+#             #     level = 1
+#
+#             # l['node_' + str(count_root) +"_root_node"] = Relationship(root_node, node.get("rela"), l['test_node_' + str(count_root)])
+#             # graph.create(l['node_' + str(count_root) +"_root_node"])
+#
+#             # node = str(node)
+#             # node = node.replace("'type'", 'type').replace("'source'", 'source').replace("'target'", 'target')
+#             # print(node + ',')
+#             # count_root += 1
+#
+# if __name__ == '__main__':
+#     web_kg('水果')
