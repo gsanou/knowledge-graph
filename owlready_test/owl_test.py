2143b549f4e9b1a3d959f7c1870d589ccf003c98
Yuhao Mao <maoyuhao92@gmail.com>
2019-11-23 02:23:57 +0800

    几个owlready的小例子
    有添加class，添加subclass，添加dataproperty

--- owlready_test/owl_test.py
+++ owlready_test/owl_test.py
@@ -1,27 +1,66 @@
 from owlready2 import *
-# path = "/Users/yuhaomao/Desktop/new_robot.owl"
-iri= "http://www.semanticweb.org/yuhaomao/ontologies/2019/9/untitled-ontology-17"
-onto = get_ontology(iri)
+import uuid
+path = "/Users/yuhaomao/Desktop/AES-CN/owlready_test/hello.rdf"
+# iri= "http://www.semanticweb.org/yuhaomao/ontologies/2019/9/untitled-ontology-17"
+onto = get_ontology(path).load()
+# print(onto.Drug)
 with onto:
     class Drug(Thing):
         pass
-    class Ingredient(Thing):
-        pass
-    class has_for_ingredient(ObjectProperty):
-        domain = [Drug]
-        range = [Ingredient]
+#     class Drug1(Thing):
+#         pass
+#     class Drug2(Thing):
+#         pass
+#     class Ingredient(Thing):
+#         pass
+#     class has_for_ingredient(ObjectProperty):
+#         domain = [Drug]
+#         range = [Ingredient]
 
+# my_drug111 = Drug("individual")
+# acetaminophen = Ingredient("acetaminophen")
+# my_drug.has_for_ingredient = [acetaminophen]
+# onto.save("hello111.rdf")
 
-my_drug = Drug("my_drug")
-acetaminophen = Ingredient("acetaminophen")
-my_drug.has_for_ingredient = [acetaminophen]
-onto.save("hello.rdf")
-
-
-
-
-# add_class() 可以通过调用的时候选择class名字并且选择父节点
-def add_class(name, parent, onto):
+# 添加单独一个class
+def add_class(name, parent,onto):
     with onto:
         exec('class {}({}): pass'.format(name, parent))
+# add_class("xxx", Thing,onto)
+
+# 创建uuid属于某个class
+def add_individual(class_name, parent_class, individual_name,onto):
+    print(individual_name)
+    print(type(individual_name))
+    with onto:
+        # exec('class {class_name}({parent}): pass'.format(class_name=class_name, parent=parent_class))
+        exec('{class_name}("{individual_name}")'.format(class_name=class_name, individual_name=individual_name))
+# add_individual("Drug",Thing, "indivu",onto)
+
+# 创建data_property
+# plan1
+def add_dataproperty(dataproperty_name,domain,range):
+    with onto:
+        onto_data_property = types.new_class(dataproperty_name, (DataProperty,))
+        onto_data_property.domain = [domain]
+        onto_data_property.range = [range]
+# add_dataproperty("name1",Drug,str)
+
+# plan2
+# with onto:
+#     class has_for_synonym(DataProperty):
+#         domain = [Thing]
+#         range = [int]
+# onto.has_for_synonym = ["acetaminophen", "paracétamol"] # []里的两个参数没什么用好像
+
+# plan3 推荐第三个
+def add_dataproperty(dataproperty_name,domain_value,range_value):
+    with onto:
+        exec('class {0}(DataProperty): \n domain=[{1}]  \n range=[{2}]'.format(dataproperty_name,domain_value,range_value))
+    onto.has_for_synonym = ["acetaminophen", "paracétamol"]
+# add_dataproperty("has_for_synonym","Thing","str")
+
+# 添加 data_property 的value
+
+onto.save("hello.rdf")
 
