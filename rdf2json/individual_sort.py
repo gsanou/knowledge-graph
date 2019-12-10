511dd25696df1321b14fd24f78c38014f0ad3d43
Yuhao Mao <maoyuhao92@gmail.com>
2019-12-10 16:18:56 +0800

    iteration string
    改了一下递归是string的情况

--- rdf2json/individual_sort.py
+++ rdf2json/individual_sort.py
@@ -1,9 +1,8 @@
 import copy
 
-a = {"1":{"2":{"3":{"4":{"5":{"6":{"7":{"8":{"9":{"10":"11"}}}}}}}}},"13":{"18":"12"}}
-b = {"1":"2","3":"4","5":"6"}
 index_list = []
 flag = False
+
 
 def iteration(a,target):
     global index_list
@@ -15,6 +14,8 @@ def iteration(a,target):
             flag = True
             index_list.append(i)
             break
+        if type(i) == str:
+            pass
         else:
             if type(a[i]) == dict or type(a[i]) == list:
                 iteration(a[i],target)
@@ -22,40 +23,19 @@ def iteration(a,target):
                 flag = True
                 index_list.append(i)
                 break
-        if flag == True:
+        if flag is True:
             index_list.append(i)
             break
     # print(index_list)
 
-# replace_item = {"11":"aaa"}
-# for i in replace_item:
-#     target = i
-# print(target)
-# iteration(a,target)
-# index_result = index_list
-# print(index_result)
-#
-# tmp = "a"
-# for i in range(len(index_result)):
-#     tmp = tmp + "[\"" + index_result[::-1][i] + "\"]"
-#
-# if index_result[0] == target:
-#     exec('{0} = [{0},"{1}"]'.format(tmp,replace_item[target]))
-# else:
-#     exec('{0} = "{1}"'.format(tmp, replace_item[target]))
-#
-#
-# print(a)
-
 def replace(main_dictionary,dictionary_name,replace_item):
     global index_list
     global flag
+    # exec('{0} = {1}'.format(dictionary_name,main_dictionary))
+    a = main_dictionary
     for i in replace_item:
         target = i
         value = replace_item[i]
-        print("ssssss")
-        print(target)
-        print(value)
 
     iteration(main_dictionary, target)
     index_result_key = copy.deepcopy(index_list)
@@ -68,27 +48,41 @@ def replace(main_dictionary,dictionary_name,replace_item):
     # print(index_list)
     iteration(main_dictionary, value)
     index_result_value = copy.deepcopy(index_list)
-    # print("""value""")
-    # print(index_result_value)
+    print("""value""")
+    print(index_result_value)
 
     if len(index_result_key) != 0 and len(index_result_value) == 0:
+        """
+        key 在dictionary里
+        value不在dictionary里
+        """
         tmp = dictionary_name
         for i in range(len(index_result_key)):
             tmp = tmp + "[\"" + index_result_key[::-1][i] + "\"]"
-
+        print("sdsdad")
+        print(tmp)
         if index_result_key[0] == target:
             exec('{0} = [{0},"{1}"]'.format(tmp, replace_item[target]))
         else:
             xxx = {target:replace_item[target]}
+            print("xxxx")
+            print(xxx)
+            print(tmp)
             exec('{0} = {1}'.format(tmp, xxx))
 
-        print(a)
+        print(main_dictionary)
 
     if len(index_result_key) == 0 and len(index_result_value) == 0:
+        """
+        key和value都不在dictionary里
+        """
         main_dictionary[target] = value
-        print(a)
+        print(main_dictionary)
 
     if len(index_result_key) != 0 and len(index_result_value) != 0:
+        """
+        key和value都在dictionary里
+        """
         t1 = main_dictionary[value]
         t2 = {value:t1}
         tmp_value = copy.deepcopy(t2)
@@ -104,11 +98,35 @@ def replace(main_dictionary,dictionary_name,replace_item):
         else:
             xxx = {target:tmp_value}
             exec('{0} = {1}'.format(tmp, xxx))
-        print(a)
+        print(main_dictionary)
+    if len(index_result_key) == 0 and len(index_result_value) != 0:
+        """
+        key不在dictionary里
+        value在dictionary里
+        """
+        tmp = copy.deepcopy(main_dictionary[value])
+        del main_dictionary[value]
 
-if __name__ == '__main__':
-    print(a)
-    replace_item = {"11":"13"}
-    replace(a,"a",replace_item)
-    # a["1"]["2"]["3"]["4"]["5"]["6"]["7"]["8"]["9"]["10"] = {"11":"13"}
-    # print(a)
+        xxx = {value:tmp}
+        main_dictionary[target] = xxx
+        print(main_dictionary)
+
+    flag = False
+    index_list = []
+
+# if __name__ == '__main__':
+    # a = {"1": {"2": {"3": {"4": {"5": {"6": {"7": {"8": {"9": {"10": "11"}}}}}}}}}, "13": {"18": "12"}}
+#     # print(a)
+#     # list = [{"11":"13"},{"11":"14"},{"19":"20"},{"30":"1"}]
+#     # # list = [{"30":"1"}]
+#     # for i in list:
+#     #     replace_item = i
+#     #     replace(a,"a",replace_item)
+
+a = {}
+list = [{"11": "13"}, {"11": "15"},{"11": "14"}, {"19": "20"}, {"30": "11"}]
+# list = [{"30":"1"}]
+for i in list:
+    replace_item = i
+    replace(a, "a", replace_item)
+print(a)
